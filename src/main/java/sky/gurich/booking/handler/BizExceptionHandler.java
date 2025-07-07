825566583ecb662fa5e4ab97c417573d2b599fa9
김준영 <goldrex123@naver.com>
2025-07-07 14:52:18 +0900

    전역 Exception Handler 처리 -> HttpMessageNotReadableException

--- src/main/java/sky/gurich/booking/handler/BizExceptionHandler.java
+++ src/main/java/sky/gurich/booking/handler/BizExceptionHandler.java
@@ -1,5 +1,7 @@
 package sky.gurich.booking.handler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sky.gurich.booking.common.ApiResponse;
 import sky.gurich.booking.common.ApiResponseCode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -37,8 +41,30 @@ public class BizExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ApiResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         log.error("handle HttpMessageNotReadableException - {}", ex.getMessage());
-        
-        ApiResponse<String> response = ApiResponse.fail(ApiResponseCode.REQUEST_BODY_NOT_READABLE, ex.getMessage());
+
+        Throwable cause = ex.getCause();
+        String message = "잘못된 Request Body 데이터 입니다";
+
+        if (cause instanceof InvalidFormatException invalidFormatException) {
+            Class<?> targetType = invalidFormatException.getTargetType();
+            String fieldName = invalidFormatException.getPath().stream()
+                    .map(ref -> ref.getFieldName())
+                    .reduce((prev, curr) -> curr)
+                    .orElse("알 수 없는 필드");
+
+            message = String.format("%s 필드는 %s 타입이어야 합니다.", fieldName, targetType.getSimpleName());
+
+            if (targetType.isEnum()) {
+                Object[] enumConstants = targetType.getEnumConstants();
+                String allowed = Arrays.stream(enumConstants)
+                        .map(o -> o.toString())
+                        .collect(Collectors.joining(","));
+
+                message += " 허용 값: [" + allowed + "]";
+            }
+        }
+
+        ApiResponse<String> response = ApiResponse.fail(ApiResponseCode.REQUEST_BODY_NOT_READABLE, message);
         return ResponseEntity.badRequest().body(response);
     }
 
