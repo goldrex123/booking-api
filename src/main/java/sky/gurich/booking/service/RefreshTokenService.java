62b23868b41245654aa6efe2592fef695beec5b6
김준영 <goldrex123@naver.com>
2025-07-18 10:30:28 +0900

    redis 설정

---
+++ src/main/java/sky/gurich/booking/service/RefreshTokenService.java
@@ -0,0 +1,26 @@
+package sky.gurich.booking.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void saveTokenOnRedis(String username, String refreshToken) {
+        stringRedisTemplate.opsForValue().set("RT:"+username, refreshToken, Duration.ofDays(1));
+    }
+
+    public String getToken(String username) {
+        return stringRedisTemplate.opsForValue().get("RT:" + username);
+    }
+
+    public void deleteToken(String username) {
+        stringRedisTemplate.delete("RT:" + username);
+    }
+}
